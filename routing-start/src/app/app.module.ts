c364178cf917eb842bdbdba9ed13622a0d9f0b9f
Rodas Zegeye <rodas.zegeye@equifax.com>
2021-03-12 16:57:58 -0500

    modify the routes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -11,9 +11,15 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router'
 
  
+const appRoute: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'servers', component: ServersComponent },
+{ path: 'users', component: UsersComponent },
 
+];
 
 
 @NgModule({
@@ -29,6 +35,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
